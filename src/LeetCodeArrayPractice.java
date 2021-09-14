8804521648e1bb826a91f6065845a9f82cf7fa53
shahim <shahim123321@gmail.com>
2021-09-15 03:17:44 +1000

    DaysConverter

--- src/LeetCodeArrayPractice.java
+++ src/LeetCodeArrayPractice.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class LeetCodeArrayPractice {
     public static void main(String[] args) {
         System.out.println(maxNumberOfBalloons("balloonballoonblloon"));
-        int []arr1 = {3,8,2,5,6,9,1,6};
-       // System.out.println(Arrays.toString(twoSum(arr1, 7)));
-        Arrays.sort(arr1);
-        displayArr(arr1);
+        int []arr1 = {2,3,7,6,10,8,4};
+       System.out.println(Arrays.toString(twoSum(arr1, 7)));
+        calcDays(200);
     }
     public static void displayArr(int[]arr){
         for(int a:arr){
@@ -38,25 +38,32 @@ public class LeetCodeArrayPractice {
 
 
     }
-//    public static int [] twoSum(int []arr, int target){
-//        int left = 0;
-//        int right = arr.length-1;
-//        ArrayList<Integer>results = new ArrayList<>();
-//        Arrays.sort(arr);
-//        int counter = 0;
-//
-//        while(left<right){
-//            if(arr[left]+arr[right]>target){
-//                right--;
-//            }else if(arr[left]+arr[right]<target){
-//                left++;
-//            }else{
-//                results.add(left,right);
-//                left++;
-//                right--;
-//            }
-//        }
-//        //int []ans = results.toArray(new int[results.size()]);
-//
-//    }
+    public static int [] twoSum(int []arr, int target){
+
+        HashMap<Integer, Integer> visitedNums = new HashMap<>();
+        for(int i = 0;i<arr.length;i++){
+            int delta = target - arr[i];
+            if(visitedNums.containsKey(delta)){
+                return new int[]{visitedNums.get(delta),i};
+            }
+            visitedNums.put(arr[i], i);
+        }
+        return new int []{-1,-1};
+    }
+    public static void calcDays(int daysToConvert){
+        int year,month,week,day;
+        if(daysToConvert<365){
+          year = 0;
+        }else{
+          year = daysToConvert/365;
+          daysToConvert-=365;
+        }
+        month = daysToConvert/30;
+        daysToConvert -= month*30;
+        week = daysToConvert/7;
+        daysToConvert-=week*7;
+        day = daysToConvert;
+
+        System.out.println(year + " years " + month + " months " + week + " weeks " + day + " days ");
+    }
 }
